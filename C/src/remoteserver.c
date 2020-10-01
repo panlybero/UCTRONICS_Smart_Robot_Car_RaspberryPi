8b9af27bb912d9c6864c213ecdd4e806731680bb
Panagiotis Lymperopoulos <panliberopoulos@gmail.com>
2020-10-01 04:01:04 -0400

    ..

--- C/src/remoteserver.c
+++ C/src/remoteserver.c
@@ -20,6 +20,16 @@
 #include <stdarg.h>
 #include <getopt.h>
 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+
+  
+#define PORT     9001 
+#define MAXLINE 1024 
+
+
 
 #include "clk.h"
 #include "gpio.h"
@@ -151,15 +161,8 @@ int main(int argc, char *argv[])
   if (ControllerInit() < 0) return -1;
 
   setup_io();
-  /*
-  ultraInit();
-  servoInit();
-  trackModeInit();
-  beepInit();
-  irInit();
-  myPWMInit();
-  GRBInit();
-  */
+  
+
 
   for(int i = 0;i<10; i++)
   {
@@ -168,6 +171,43 @@ int main(int argc, char *argv[])
     sleep(1);
   }
   stop();
+
+  int sockfd; 
+  char buffer[MAXLINE]; 
+  char *hello = "Hello from server"; 
+  struct sockaddr_in servaddr, cliaddr; 
+    
+  // Creating socket file descriptor 
+  if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+      perror("socket creation failed"); 
+      exit(EXIT_FAILURE); 
+  } 
+    
+  memset(&servaddr, 0, sizeof(servaddr)); 
+  memset(&cliaddr, 0, sizeof(cliaddr)); 
+    
+  // Filling server information 
+  servaddr.sin_family    = AF_INET; // IPv4 
+  servaddr.sin_addr.s_addr = INADDR_ANY; 
+  servaddr.sin_port = htons(PORT); 
+    
+  // Bind the socket with the server address 
+  if ( bind(sockfd, (const struct sockaddr *)&servaddr,  
+          sizeof(servaddr)) < 0 ) 
+  { 
+      perror("bind failed"); 
+      exit(EXIT_FAILURE); 
+  } 
+    
+  int len, n; 
+
+  len = sizeof(cliaddr);  //len is value/resuslt 
+
+  n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
+              MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
+              &len); 
+  buffer[n] = '\0'; 
+  printf("Client : %s\n", buffer); 
   /*
   
  
