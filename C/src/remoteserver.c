50e1c8b3f4534decd90e20fbbe7d6939c49174fb
Panagiotis Lymperopoulos <panliberopoulos@gmail.com>
2020-10-03 01:41:27 -0400

    ..

--- C/src/remoteserver.c
+++ C/src/remoteserver.c
@@ -204,7 +204,7 @@ int main(int argc, char *argv[])
               MSG_WAITALL, ( struct sockaddr *) &cliaddr, 
               &len); 
     buffer[n] = '\0'; 
-    printf("Client : %s\n", buffer[0]); 
+    printf("Client : %s\n", buffer); 
     if(buffer[0]=='w'){
       for(int i = 0;i<5; i++)
       {
