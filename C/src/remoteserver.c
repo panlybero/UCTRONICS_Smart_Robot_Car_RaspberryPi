4d248e4660225c4e8fbcae33b059a0e863f2d40b
Panagiotis Lymperopoulos <panliberopoulos@gmail.com>
2020-10-03 19:43:30 -0400

    ..

--- C/src/remoteserver.c
+++ C/src/remoteserver.c
@@ -162,15 +162,15 @@ int main(int argc, char *argv[])
 
   setup_io();
   myPWMInit();
-  ultraInit();
+  //ultraInit();
   //servoInit();
   //trackModeInit();
-  beepInit();
+  //beepInit();
   //irInit();
   //GRBInit();
   pthread_t t1, t2;
   //creat two thread
-  pthread_create(&t1, NULL, fun1, NULL);
+  //pthread_create(&t1, NULL, fun1, NULL);
   //pthread_create(&t2, NULL, fun2, NULL);
 
   int sockfd; 
