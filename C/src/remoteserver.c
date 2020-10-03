52824c717fc93686a84b461167b5af90f9fcc914
Panagiotis Lymperopoulos <panliberopoulos@gmail.com>
2020-10-03 19:45:48 -0400

    ..

--- C/src/remoteserver.c
+++ C/src/remoteserver.c
@@ -171,7 +171,7 @@ int main(int argc, char *argv[])
   pthread_t t1, t2;
   //creat two thread
   //pthread_create(&t1, NULL, fun1, NULL);
-  pthread_create(&t2, NULL, fun2, NULL);
+  //pthread_create(&t2, NULL, fun2, NULL);
 
   int sockfd; 
   //char buffer[MAXLINE]; 
